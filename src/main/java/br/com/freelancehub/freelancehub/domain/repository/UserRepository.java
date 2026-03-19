e61de40d2519f68d9635f9aa441fd8e8b403a7f9
Eduardo <duba.noda737@gmail.com>
2026-03-19 08:52:16 -0400

    feat: adicionado função buscar usuario por email

--- src/main/java/br/com/freelancehub/freelancehub/domain/repository/UserRepository.java
+++ src/main/java/br/com/freelancehub/freelancehub/domain/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package br.com.freelancehub.freelancehub.domain.repository;
 
 import br.com.freelancehub.freelancehub.domain.User;
+import br.com.freelancehub.freelancehub.domain.valueobjects.Email;
 
 import java.util.Optional;
 
@@ -9,5 +10,7 @@ public interface UserRepository {
     void save (User user);
 
     Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
 
 }
