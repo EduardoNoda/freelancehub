9e316b1861488fe35071647206e1b3ffccb7bbd5
Eduardo <duba.noda737@gmail.com>
2026-03-23 15:15:51 -0400

    feat: criado um segundo construtor pedindo parametro id para busca de usuarios no banco de dados e desenvolvido métodos de acesso ao nome, sobrenome, hash de senha e email em string

--- src/main/java/br/com/freelancehub/freelancehub/domain/User.java
+++ src/main/java/br/com/freelancehub/freelancehub/domain/User.java
@@ -32,12 +32,37 @@ public class User {
         this.password = password;
     }
 
-    public String getFullName(String name, String lastName) {
-        return name + " " + lastName;
+    public User (
+            Long id,
+            String name,
+            String lastName,
+            Email email,
+            String password
+    ) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Name cannot be null");
+
+        if (lastName == null || lastName.isBlank())
+            throw new IllegalArgumentException("lastName cannot be null");
+
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
     }
 
-    public String getPasswordHash() {
-        return this.password;
+
+    public String getName () { return this.name = name; }
+
+    public String getLastName () { return this.lastName = lastName; }
+
+    public String getPasswordHash() { return this.password; }
+
+    private Email getEmail() {
+        return this.email;
     }
+
+    public String getEmailAdress() { return this.email.email(); }
 
 }
