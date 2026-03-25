e0e4e389fed05fd848fa172a397506801ec57501
Eduardo <duba.noda737@gmail.com>
2026-03-25 09:53:53 -0400

    fix: annotation Service para indicar para o spring que é uma implementação - dava erro de execução

--- src/main/java/br/com/freelancehub/freelancehub/infrastructure/security/BCryptPasswordEncoderAdapter.java
+++ src/main/java/br/com/freelancehub/freelancehub/infrastructure/security/BCryptPasswordEncoderAdapter.java
@@ -2,7 +2,9 @@ package br.com.freelancehub.freelancehub.infrastructure.security;
 
 import br.com.freelancehub.freelancehub.application.ports.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BCryptPasswordEncoderAdapter implements PasswordEncoder {
     @Override
     public String encoder(String plainPassword) {
