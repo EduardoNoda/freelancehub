b29748a50ffb54d1a918df1efecfd589de768f48
Eduardo <duba.noda737@gmail.com>
2026-04-13 21:58:12 -0400

    feat: added new methods for time management, adding projects, and changing project status.

--- src/main/java/br/com/freelancehub/freelancehub/infrastructure/config/UseCaseConfig.java
+++ src/main/java/br/com/freelancehub/freelancehub/infrastructure/config/UseCaseConfig.java
@@ -2,12 +2,13 @@ package br.com.freelancehub.freelancehub.infrastructure.config;
 
 import br.com.freelancehub.freelancehub.application.ports.PasswordEncoder;
 import br.com.freelancehub.freelancehub.application.ports.TokenService;
-import br.com.freelancehub.freelancehub.application.usecases.AuthUserUseCase;
-import br.com.freelancehub.freelancehub.application.usecases.CreateUserUseCase;
-import br.com.freelancehub.freelancehub.application.usecases.LoginUserUseCase;
+import br.com.freelancehub.freelancehub.application.usecases.*;
+import br.com.freelancehub.freelancehub.domain.repository.ProjectRepository;
 import br.com.freelancehub.freelancehub.domain.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
 
 @Configuration
 public class UseCaseConfig {
@@ -28,6 +29,19 @@ public class UseCaseConfig {
     @Bean
     public AuthUserUseCase authUserUseCase(UserRepository userRepository, TokenService tokenService) {
         return new AuthUserUseCase(userRepository, tokenService);
+    }
+
+    @Bean
+    public Clock clock () { return Clock.systemUTC(); }
+
+    @Bean
+    public CreateProjectUseCase createProjectUseCase(ProjectRepository projectRepository, UserRepository userRepository, Clock clock) {
+        return new CreateProjectUseCase(projectRepository, userRepository, clock);
+    }
+
+    @Bean
+    public ChangeStatusProjectUseCase changeStatusProjectUseCase(ProjectRepository projectRepository, UserRepository userRepository, Clock clock) {
+        return new ChangeStatusProjectUseCase(projectRepository, userRepository, clock);
     }
 
 }
